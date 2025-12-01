0a6e8ddccaf8be45dac774a28d4c450704ced91f
Richard Karth <richard.k.triumph@gmail.com>
2025-12-01 11:00:44 +0100

    Added the menu dialog and switch case for option input

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -4,8 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadLine();
+            Console.WriteLine(" --Todo App--");
+            Console.WriteLine(" 1. Add Chore");
+            Console.WriteLine(" 2. Remove Chore");
+            Console.WriteLine(" 3. View Chores");
+            Console.WriteLine(" 4. Exit");
+            Console.Write(" Select an option: ");
+
+            var option = Console.ReadLine();
+            if (int.TryParse(option, out int num))
+            {
+                switch (num)
+                {
+                    case 1:
+                        AddChore();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        Console.WriteLine(" Exiting...");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Invalid option. Please enter a number.");
+            }
         }
+
+        static void AddChore()
+        {
+            Console.Clear();
+            Console.WriteLine(" --Add Chore--");
+            Console.Write("Enter Chore Description:");
+            var choreName = Console.ReadLine();
+
+        }
+
     }
 }
